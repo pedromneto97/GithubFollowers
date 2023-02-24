e22dd22239a1c660dfd4f55ec72758b2f23916cf
Pedro Neto <pedromneto97@gmail.com>
2023-02-24 19:03:21 -0300

    feat: add glide module

---
+++ app/src/main/java/br/eng/pedro_mendes/github_followers/ui/core/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package br.eng.pedro_mendes.github_followers.ui.core
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
